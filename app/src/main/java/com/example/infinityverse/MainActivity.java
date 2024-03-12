1934a2ef21d832953e76b997325901994d1ebd06
Vivek <viveksai.perikala@gmail.com>
2024-03-12 18:49:58 +0530

    NumberGeneratorApp

---
+++ app/src/main/java/com/example/infinityverse/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.infinityverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int val = random.nextInt(11-1)+1;
+                txt.setText(Integer.toString(val));
+            }
+        });
+
+    }
+}
